07a0026a66bb093ea1937eec25016415247f4686
Andrey <andrey2002335@gmail.com>
2020-03-31 23:35:23 +0300

    Update Item class

--- LabaHashtable/Item.cs
+++ LabaHashtable/Item.cs
@@ -1,20 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LabaHashtable
 {
-    class Item
+    
+    public class Item
     {
-        public string Key { get; private set;}
+        
+        public string Key { get; private set; }
+
+        
         public string Value { get; private set; }
+
+        
         public Item(string key, string value)
         {
-            key = Key;
-            value = Value;
+            Key = key;
+            Value = value;
         }
+        
         public override string ToString()
         {
             return Key;
