ccc47d5cbc782149aeaa32ded62868d77e92f680
Dariia Kariavka <koalaansthen@gmail.com>
2020-06-08 12:35:01 +0300

    implement list with node/ instead of dictionary

--- LabaHashtable/Hashtable.cs
+++ LabaHashtable/Hashtable.cs
@@ -14,8 +14,62 @@ namespace LabaHashTable
 
         private Dictionary<int, List<Item>> _items = null;
 
-        public IReadOnlyCollection<KeyValuePair<int, List<Item>>> Items => _items?.ToList()?.AsReadOnly();
-        
+        //public IReadOnlyCollection<KeyValuePair<int, List<Item>>> Items => _items?.ToList()?.AsReadOnly();
+        public class Node
+{
+    public Node next;
+    public Object data;
+}
+
+public class LinkedList
+{
+    private Node head;
+
+    public void printAllNodes()
+    {
+        Node current = head;
+        while (current != null)
+        {
+            Console.WriteLine(current.data);
+            current = current.next;
+        }
+    }
+
+    public void AddFirst(Object data)
+    {
+        Node toAdd = new Node();
+
+        toAdd.data = data;
+        toAdd.next = head;
+
+        head = toAdd;
+    }
+
+    public void AddLast(Object data)
+    {
+        if (head == null)
+        {
+            head = new Node();
+
+            head.data = data;
+            head.next = null;
+        }
+        else
+        {
+            Node toAdd = new Node();
+            toAdd.data = data;
+
+            Node current = head;
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+
+            current.next = toAdd;
+        }
+    }
+}
+
         public HashTable()
         {
            
@@ -126,7 +180,7 @@ namespace LabaHashTable
                 {
                     if(i.Key == key)
                     {
-                        Console.WriteLine("Counter:"+counter);
+                        Console.WriteLine("Counter:"+counter+"VALUE"+hash);
                         return i.Value;
                     }
                     else
